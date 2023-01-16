2529362b1c7747e9c3686f05a397494b31eec15f
subhran <subhranshu497@gmail.com>
2023-01-16 16:13:56 -0700

    Linked List Program

---
+++ src/com/algo/LinkedList/Node.java
@@ -0,0 +1,10 @@
+package com.algo.LinkedList;
+
+public class Node {
+    int data;
+    Node next =null;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
---
+++ src/com/algo/LinkedList/RemoveDupLL.java
@@ -0,0 +1,53 @@
+package com.algo.LinkedList;
+
+import java.util.HashSet;
+
+public class RemoveDupLL {
+    public static void main(String[] args) {
+        Node n1 = new Node(2);
+        Node n2 = new Node(1);
+        Node n3 = new Node(3);
+        Node n4 = new Node(3);
+        Node n5 = new Node(2);
+        Node n6 = new Node(0);
+
+        //linked node
+        n1.next =n2;
+        n2.next =n3;
+        n3.next =n4;
+        n4.next =n5;
+        n5.next =n6;
+        printLinkedList(n1);
+        removeDupFromLL(n1);
+        System.out.println();
+        System.out.println("Post removal of Dups ");
+        printLinkedList(n1);
+
+    }
+
+ //print LL
+    public static void printLinkedList(Node head){
+        if(head == null) return;
+        System.out.print(head.data +" "+"-->");
+        Node curr_node = head.next;
+        while (curr_node != null){
+            System.out.print(curr_node.data +" "+"-->");
+            curr_node = curr_node.next;
+        }
+    }
+    //remove Dups
+    public static void removeDupFromLL(Node node) {
+        HashSet<Integer> hs = new HashSet<>();
+        Node prev = null;
+        while(node.next != null){
+            if(!hs.contains(node.data)){
+                hs.add(node.data);
+                prev = node;
+            }
+            else{
+                prev.next = node.next;
+            }
+            node = node.next;
+        }
+    }
+}
